2480fb775dd5efc5d16d04510dcec6d037f7b299
krystle Mensah <web-development@krystlemensah.co.uk>
2025-04-01 18:19:31 +0100

    afteroon 1st Arpil 2025

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-
+//This is index.php. Blog screen
 get_header(); ?>
 
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/pageBanner.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Rgen music Blog</h1>
+    <h1 class="page-banner__title">RGENMUSIC BLOG</h1>
     <div class="page-banner__intro">
-      <p>Your Gateway to Music Culture. This is index.php. Blog screen</p>
+      <p>Your Gateway to Music Culture.</p>
     </div>
   </div>
 </div>
@@ -18,11 +18,8 @@ get_header(); ?>
   while (have_posts()) {
     // Set up the current post data. This prepares template tags (e.g., the_title(), the_permalink()) to output information about the post.
     the_post(); ?>
-    <!-- Container for an individual post item -->
     <div class="post-item">
-      <!-- Post title: displayed as a clickable headline linking to the single post page -->
       <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <!-- Meta information about the post -->
       <div class="metabox">
         <!-- Shows the author with a link to their archive page, Lists all categories the post belongs to, separated by commas -->
         <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
